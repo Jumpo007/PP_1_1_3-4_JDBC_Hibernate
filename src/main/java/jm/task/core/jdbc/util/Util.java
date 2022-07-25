6645b49c8caf3d6a2b09b4bf8a1886740206fc2c
Jumpo007 <78562147+Jumpo007@users.noreply.github.com>
2022-07-25 16:17:18 +0300

    Com6

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -6,16 +6,16 @@ import java.sql.SQLException;
 public class Util {
     // реализуйте настройку соеденения с БД
     private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
-    private static String dbURL = "jdbc:mysql://localhost:3306/mydbtest";
-    private static String dbUsername = "root";
+    private static final String URL = "jdbc:mysql://localhost:3306/mydbtest";
+    private static final String USERNAME = "root";
 
-    private static String dbPassword = "roots";
+    private static final String PASSWORD = "roots";
 
     public static Connection getConnection() {
         Connection connection;
         try {
             Class.forName(DRIVER);
-            connection = DriverManager.getConnection(dbURL, dbUsername, dbPassword);
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
         } catch (SQLException | ClassNotFoundException e) {
             throw new RuntimeException(e);
         }
